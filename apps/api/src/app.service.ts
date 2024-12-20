8aa063478d93ad0fb9da589e1410fe4087bf3e2f
SantoshPayasi <santoshpayasi78@gmail.com>
2024-12-21 01:46:52 +0530

    Initial commit

---
+++ apps/api/src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { add } from '@parkingspace/sample-lib';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return `Hello World! ${add(2, 3)}`;
+  }
+}
